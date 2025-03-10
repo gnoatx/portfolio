3e98350ccf72b9c24227625ae54cc7d82d823bd7
Victor Gnoato <85642178+gnoatx@users.noreply.github.com>
2025-03-10 15:21:30 -0300

    Bio atualizada

--- src/components/Bio/index.tsx
+++ src/components/Bio/index.tsx
@@ -7,7 +7,7 @@ export default function Bio() {
             <p>Programador Front-End com especialização em React.</p>
             <p>Criatividade, organização, proatividade, vontade de encarar novos desafios.</p>
             <p>Formado no curso de <b>Programador Fullstack</b> pelo Firjan Senai.</p>
-            <p>Cursando o curso <b>Oracle ONE - Especialização Front-End</b> pela Alura.</p>
+            <p>Formado no curso <b>Oracle ONE - Especialização Front-End</b> pela Alura.</p>
             <BgCredit at='kxvn_lx' name='Kevin Laminto' photoId='7PqRZK6rbaE' />
         </section>
     )
