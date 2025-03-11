3c04b896bc67249c8a42c5fa2daa35aaedf3784d
Victor Gnoato <85642178+gnoatx@users.noreply.github.com>
2025-03-11 15:14:03 -0300

    Fix: Responsividade da foto de perfil

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react'
 import BgCredit from '../BgCredit'
 import style from './Header.module.css'
 
 export default function Header() {
+    const [profileWidth, setProfileWidth] = useState(window.innerWidth <= 1000 ? 150 : 300);
+
+    useEffect(() => {
+        const handleResize = () => setProfileWidth(window.innerWidth <= 1000 ? 150 : 300)
+        window.addEventListener("resize", handleResize)
+        return () => window.removeEventListener("resize", handleResize)
+    }, [])
+
     return (
         <header className={style.header}>
             <div className={style['info-block']}>
@@ -10,7 +19,7 @@ export default function Header() {
                     <h1 className={style.name}>Victor Gnoato</h1>
                 </div>
                 <div className={style['image-container']}>
-                    <img className={style['profile-picture']} src="/assets/profile.png" alt="Foto de perfil do desenvolvedor" height={window.innerWidth <= 1000 ? 150 : 300} />
+                    <img className={style['profile-picture']} src="/assets/profile.png" alt="Foto de perfil do desenvolvedor" height={profileWidth} />
                 </div>
             </div>
             <BgCredit at='jezar' name='Jezael Melgoza' photoId='layMbSJ3YOE' />
